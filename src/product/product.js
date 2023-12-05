8815f02619ecdfa56ccf37caf70c3bb6dedfa8a3
Rietdijk <c40191@eon.com>
2023-12-05 14:15:58 +0100

    Implementation new feature: animal. In product.js, pact.setup.js, and product.repository.js.

--- src/product/product.js
+++ src/product/product.js
@@ -1,9 +1,10 @@
 class Product {
-    constructor(id, type, name, version) {
+    constructor(id, type, name, version, animal) {
         this.id = id;
         this.type = type;
         this.name = name;
         this.version = version;
+        this.animal = animal;
     }
 }
 
